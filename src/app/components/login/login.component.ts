b2d32413a1e5afeb1e4f17c696d0bdc744601b2b
darsay8 <darsay.one@gmail.com>
2024-11-18 16:41:24 -0300

    add login component with form validation and error handling; implement authentication service

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  // users: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private el: ElementRef,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submitForm(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.loginUser(email, password);
+      this.router.navigate(['/']);
+    } else {
+      this.markFormGroupTouched(this.loginForm);
+    }
+  }
+
+  markFormGroupTouched(formGroup: FormGroup): void {
+    Object.values(formGroup.controls).forEach((control) => {
+      control.markAsTouched();
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
+}
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../models/models';
+import { UserService } from '../user/user.service';
+import { StorageService } from '../storage/storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private currentUser: User | null = null;
+
+  constructor(
+    private storageService: StorageService,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  registerUser(
+    email: string,
+    password: string,
+    username: string,
+    role: string
+  ): void {
+    this.userService.getUsers().subscribe(
+      (users: User[]) => {
+        const id = this.generateUniqueId(users);
+        const newUser = { id, email, password, username, role };
+        users.push(newUser);
+        const usersForSave = { data: { users } };
+        this.userService.addUser(usersForSave).subscribe(
+          () => {
+            this.loginUser(email, password);
+          },
+          (error) => {
+            console.error('Error at register:', error);
+          }
+        );
+
+        this.storageService.setItem('users', users);
+      },
+      (error) => {
+        console.error('Error getting users:', error);
+      }
+    );
+  }
+
+  loginUser(email: string, password: string): boolean {
+    this.userService.getUsers().subscribe(
+      (users: User[]) => {
+        const user = users.find(
+          (u) => u.email === email && u.password === password
+        );
+        if (user) {
+          const { id, username, email } = user;
+          return true;
+        } else {
+          return false;
+        }
+      },
+      (error) => {
+        console.error('Error fetching users', error);
+        return false;
+      }
+    );
+    return false;
+  }
+
+  logoutUser(): void {
+    this.storageService.removeItem('currentUser');
+    this.router.navigate(['/login']);
+  }
+
+  getCurrentUser(): User | null {
+    return (
+      this.currentUser || this.storageService.getItem('currentUser') || null
+    );
+  }
+
+  private generateUniqueId(users: User[]): number {
+    if (users.length > 0) {
+      const maxId = Math.max(...users.map((user) => user.id));
+      return maxId + 1;
+    } else {
+      return 1;
+    }
+  }
+}
