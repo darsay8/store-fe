b5d66e56dd77d74bf0eafe178127ff036caf1a2c
darsay8 <darsay.one@gmail.com>
2024-11-18 17:02:30 -0300

    add recover component route; update login component template with title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { RecoverComponent } from './components/recover/recover.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'recover', component: RecoverComponent },
 ];
