8c3d8aa34a4d29415352a4c734fd4c7958ce8347
darsay8 <darsay.one@gmail.com>
2024-12-09 22:20:03 -0300

    add testing configuration with Karma; include coverage reporting and SonarQube integration; update .gitignore and add .nvmrc

---
+++ karma.conf.js
@@ -0,0 +1,50 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-safari-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    files: [
+      // Include all the test files and source files
+      { pattern: "src/**/*.spec.ts", watched: false },
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage"),
+      subdir: ".",
+      reporters: [
+        { type: "html", subdir: "html" },
+        { type: "lcov", subdir: "lcov" },
+        { type: "text-summary" },
+      ],
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    // browsers: ["Safari"],
+    browsers: [],
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
